095f81aed72c3903cc9ecb23ba4b42ee4f4ec3e6
MintyRoma <srdhe4@yandex.ru>
2022-09-14 18:40:23 +0300

    Added Task 3.1

---
+++ 3.1 Zaem/3.1 Zaem.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main()
+{
+	setlocale(0, "");
+	double Sum, Period, Percent;
+	
+	std::cout << "Введите сумму заема: ";
+	std::cin >> Sum;
+
+	std::cout << "Введите срок заема: ";
+	std::cin >> Period;
+
+	std::cout << "Введите процентную ставку: ";
+	std::cin >> Percent;
+
+	if (Sum < 0 || Period < 0 || Percent < 0)
+	{
+		std::cout << "Введены отрицательные значения";
+		return 0;
+	}
+
+	double SomeR = Percent / 100;
+
+	double MonthPay = (Sum*SomeR*std::pow(1+SomeR,2)) / (12*(std::pow(1+SomeR,2)-1));
+	std::cout << "Ежемесячный платеж: " << MonthPay;
+}
