607a15f79909f38da0b99dc002a2b21561b45d0c
MintyRoma <srdhe4@yandex.ru>
2022-09-20 23:01:47 +0300

    Ну типа сделан Task 3.2

---
+++ 3.2 Ssuda/3.2 Ssuda.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(0, "");
+
+	double percent, sum, years, monthpay;
+	
+	std::cout << "Введите сумму заема: ";
+	std::cin >> sum;
+
+	std::cout << "Введите срок заема в годах: ";
+	std::cin >> years;
+
+	std::cout << "Введите месяную выплату: ";
+	std::cin >> monthpay;
+
+	//АХТУНГ
+	//НИЖЕ ПРЕДСТАВЛЕН САМЫЙ БЛЕВОТНЫЙ МЕТОД РАССЧЕТА!!!
+	percent = 0.00001;
+	while ((sum * percent / 100 * pow((1 + percent / 100), years)) / (12 * (pow((1 + percent / 100), years) - 1)) < monthpay)
+	{
+		percent += 0.00001;
+	}
+	std::cout << "Процентная ставка: " << percent << "%";
+}
