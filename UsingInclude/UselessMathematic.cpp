d9b85c8030a6443b7a838a9eeb9b5efc3243cc56
MintyRoma <srdhe4@yandex.ru>
2022-09-24 15:04:53 +0300

    Added bullshit

---
+++ UsingInclude/UselessMathematic.cpp
@@ -0,0 +1,4 @@
+int Sum(int A, int B)
+{
+	return A + B;
+}
---
+++ UsingInclude/UsingInclude.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+#include "UselessMathematic.h" //Без этого не пашет, я бы объяснил это на теме функций
+
+int main()
+{
+	int A, B;
+	std::cin >> A >> B;
+	std::cout << Sum(B, A);
+}
