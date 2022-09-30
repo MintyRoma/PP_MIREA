3b16cfc6d0f5622a7d5938dace5d08c230de6f85
MintyRoma <srdhe4@yandex.ru>
2022-10-01 01:24:28 +0300

    Code: Completed Task 3.5 +Добавлена реализация MergeSort

--- 3.5 Sorting/3.5 Sorting.cpp
+++ 3.5 Sorting/3.5 Sorting.cpp
@@ -2,6 +2,9 @@
 #include <string>
 #include <random>
 #include <array>
+#define amount 31
+
+std::string MergeSort(std::string arr);
 
 int main()
 {
@@ -11,7 +14,7 @@ int main()
 	std::cout << "Генерация случайных букв...\n";
 	std::string base = "";
 
-	for (int i = 0; i < 30; i++)
+	for (int i = 0; i < amount; i++)
 	{
 		int charnum = std::rand() % 26;
 		char c = 'a'+charnum;
@@ -19,12 +22,59 @@ int main()
 	}
 	std::cout << "Результат: \n" << base;
 	std::cout << "\nСортировка..." << std::endl;
+	std::cout << MergeSort(base);
+}
 
-	std::string res;
-	for(int i=0;i<30;i++)
+std::string MergeSort(std::string arr)
+{
+	std::string firstpart;
+	std::string secondpart;
+	
+	if (arr.length() > 2)
 	{
-		for (int j = i+1;j<30;j++)
-
+		firstpart = MergeSort(arr.substr(0,arr.length()/2));
+		secondpart = MergeSort(arr.substr(arr.length()/2, arr.length()));
 	}
+	else
+	{
+		if (arr.length() == 2)
+		{
+			if (arr[0] > arr[1])
+			{
+				char c = arr[0];
+				arr[0] = arr[1];
+				arr[1] = c;
+			}
+		}
+		return arr;
+	}
+	std::string part;
+	int cache = firstpart.length() + secondpart.length();
+	for (int i=0;i<cache;i++)
+	{
+		if (firstpart.empty()) 
+		{
+			part += secondpart[0];
+			secondpart.erase(0, 1);
+			continue;
+		}
+		else if (secondpart.empty())
+		{
+			part += firstpart[0];
+			firstpart.erase(0, 1);
+			continue;
+		}
 
+		if (firstpart[0] < secondpart[0])
+		{
+			part += firstpart[0];
+			firstpart.erase(0, 1);
+		}
+		else
+		{
+			part += secondpart[0];
+			secondpart.erase(0, 1);
+		}
+	}
+	return part;
 }
