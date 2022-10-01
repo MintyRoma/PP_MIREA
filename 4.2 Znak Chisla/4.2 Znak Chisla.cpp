7f49840e26739d00114ce157316cd2a11578c142
MintyRoma <srdhe4@yandex.ru>
2022-10-01 20:36:06 +0300

    Code: Competed Task 4.2 +Создано и завершено задание 4.2

---
+++ 4.2 Znak Chisla/4.2 Znak Chisla.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+int DefineSymbol(int num);
+
+int main()
+{
+	while (true)
+	{
+		int num;
+		std::cin >> num;
+		std::cout << DefineSymbol(num) << std::endl;
+	}
+}
+
+int DefineSymbol(int num)
+{
+	if (num > 0) return 1;
+	if (num < 0)return -1;
+	else return 0;
+}
