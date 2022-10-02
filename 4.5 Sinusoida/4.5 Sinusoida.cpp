fafd3543fa64870a8d54fa74cf3fc6199dca75cb
MintyRoma <srdhe4@yandex.ru>
2022-10-02 21:00:44 +0300

    Code: Created Task 4.5 +Создан код для таска, требуется доработка.

---
+++ 4.5 Sinusoida/4.5 Sinusoida.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <cmath>
+
+#define point '@'
+#define empty ' '
+#define XO '-'
+#define YO '|'
+#define center '+'
+
+void DrawSin(double scale);
+
+int main()
+{
+	//setlocale(0,"");
+	DrawSin(2.5);
+}
+
+void DrawSin(double scale)
+{
+	double startpoint = -2*scale;
+	double endpoint = 2*scale;
+	for (double Y = 2;Y>-2;Y-=0.25)
+	{
+		for (double X = startpoint; X < endpoint; X += 0.25)
+		{
+			double res = sin(X);
+			//std::cout << res;
+			if (std::sin(Y - 0.25) < res && std::sin(Y + 0.25) > res) std::cout << point;
+			else if (X == 0 && Y == 0) std::cout << center;
+			else if (X == 0) std::cout << YO;
+			else if (Y == 0) std::cout << XO;
+			else std::cout << empty;
+		}
+		std::cout << std::endl;
+	}
+}
