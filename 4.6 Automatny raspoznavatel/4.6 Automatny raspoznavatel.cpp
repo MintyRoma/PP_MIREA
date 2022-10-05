5081345157f91b2941ad8a68af3d55c8addb873d
MintyRoma <srdhe4@yandex.ru>
2022-10-05 18:28:23 +0300

    Code: Completed task 4.6 +Создано и решено задание 4.6

---
+++ 4.6 Automatny raspoznavatel/4.6 Automatny raspoznavatel.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <string>
+
+int DefineNumber(char num);
+
+int main()
+{
+    setlocale(0, "");
+    std::string buffer;
+    std::cin >> buffer;
+    int result = 0;
+    for (int i = 0; i < buffer.length(); i++)
+    {
+        if (i == buffer.length() - 1)
+        {
+            result += DefineNumber(buffer[i]);
+            continue;
+        }
+        if (DefineNumber(buffer[i]) < DefineNumber(buffer[i + 1]))
+        {
+            result += (DefineNumber(buffer[i + 1]) - DefineNumber(buffer[i]));
+            i++;
+        }
+        else
+        {
+            result += DefineNumber(buffer[i]);
+        }
+    }
+    std::cout << result;
+}
+
+int DefineNumber(char num)
+{
+    switch (num)
+    {
+
+    case 'M':
+        return 1000;
+        break;
+
+    case 'D':
+        return 500;
+        break;
+
+    case 'C':
+        return 100;
+        break;
+
+    case 'L':
+        return 50;
+        break;
+
+    case 'X':
+        return 10;
+        break;
+
+    case 'V':
+        return 5;
+        break;
+
+    case 'I':
+        return 1;
+        break;
+
+    default:
+        std::cout << "Не верное число";
+        exit(0);
+        break;
+    }
+}
