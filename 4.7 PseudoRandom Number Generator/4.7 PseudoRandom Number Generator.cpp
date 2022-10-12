95e7295287365af49b4a054421f98f6048094ee0
MintyRoma <srdhe4@yandex.ru>
2022-10-12 18:21:25 +0300

    Code: Completed task 4.7 +Создано и решено задание 4.7

---
+++ 4.7 PseudoRandom Number Generator/4.7 PseudoRandom Number Generator.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+int Gen1(int previous);
+int Gen2(int previous);
+
+int main()
+{
+	setlocale(0, "");
+	int gen = 0;
+	while (gen != 1 && gen != 2)
+	{
+		std::cout << "Выберите генератор\n"\
+			"1. Вариант I\n"\
+			"2. Вариант II\n"\
+			"Ответ: ";
+		std::cin >> gen;
+	}
+	int amount;
+	std::cout << "Введите кол-во генерируемых чисел: ";
+	std::cin >> amount;
+	int previous=0;
+	for (int i = 0; i < amount; i++)
+	{
+		if (gen == 1)previous = Gen1(previous);
+		else previous = Gen2(previous);
+		std::cout << i+1<<"."<<previous<<std::endl;
+	}
+}
+
+int Gen1(int previous)
+{
+	int m = 37;
+	int b = 3;
+	int c = 64;
+	return (m * previous + b) % c;
+}
+
+int Gen2(int previous)
+{
+	int m = 25713;
+	int b = 13849;
+	int c = 65537;
+	return (m * previous + b) % c;
+}
