19c93db53efff5805c1a4bbadcb9f6afbf539ad1
MintyRoma <srdhe4@yandex.ru>
2022-10-19 19:05:52 +0300

    Code: Completed Task 5.2 +Создано и решено задание 5.2

---
+++ 5.2 Resheto Erestofena/5.2 Resheto Erestofena.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+	setlocale(0, "");
+	int Num;
+	std::cout << "Введите конечное число: ";
+	std::cin >> Num;
+
+	std::vector<int> order;
+
+	for (int i = 2; i <= Num; i++) order.push_back(i);
+	
+	int index = 0;
+	while (index!=order.size())
+	{
+		int reference = order[index];
+		int remover = -1;
+		for (int i=index+1;i<order.size();i++)
+		{
+			if (order[i] % reference == 0)
+			{
+				remover = i;
+				break;
+			}
+		}
+		if (remover != -1) 
+		{
+			order.erase(order.begin()+remover);
+		}
+		else
+		{
+			index++;
+		}
+	}
+
+	for (int c : order) std::cout << c<<" ";
+}
