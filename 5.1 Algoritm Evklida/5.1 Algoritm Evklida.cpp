285dc51fff485fdf7dfcd64b049bb739659760fb
MintyRoma <srdhe4@yandex.ru>
2022-10-19 16:58:22 +0300

    Code: Completed Task 5.1 +Создано и решено задание 5.1

---
+++ 5.1 Algoritm Evklida/5.1 Algoritm Evklida.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <cmath>
+
+int EvklidMinus(int A, int B);
+int EvklidMod(int A, int B);
+
+int main()
+{
+	setlocale(0, "");
+	int numA, numB;
+	std::cout << "Введите первое число: ";
+	std::cin >> numA;
+	std::cout << "Введите второе число: ";
+	std::cin >> numB;
+	
+	
+	std::cout << EvklidMinus(numA, numB)<<std::endl;
+	std::cout << EvklidMod(numA, numB);
+}
+
+int EvklidMinus(int A, int B)
+{
+	while (A != B && A != 0)
+	{
+		if (A > B) A -= B;
+		else B -= A;
+	}
+	return A;
+}
+
+int EvklidMod(int A, int B)
+{
+	bool parity = true;
+	while (A != B && A != 0)
+	{
+		if (parity)
+		{
+			if (A % B == 0) return B;
+			else A%=B;
+			parity = !parity;
+			continue;
+		}
+		else
+		{
+			if (A % B == 0) return A;
+			else B %= A;
+			parity = !parity;
+			continue;
+		}
+		
+	}
+}
