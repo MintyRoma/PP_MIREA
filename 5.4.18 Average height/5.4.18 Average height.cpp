4ba6ffe9b8e812e101f2636db0c105fe6384e863
MintyRoma <srdhe4@yandex.ru>
2022-10-26 14:57:36 +0300

    Code: Completed Task 5.4.18 +Создано и решено задание 5.4.18

---
+++ 5.4.18 Average height/5.4.18 Average height.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+	setlocale(0, "");
+	std::vector<int> heights;
+	std::cout << "***Анализ роста учеников***"<<std::endl;
+	std::cout << "Введите рост (см) и нажмите <Enter>."\
+		"\nДля завершения введите 0 и нажмите <Enter>"<<std::endl;
+	double buf;
+	std::cin >> buf;
+	while (buf != 0)
+	{
+		heights.push_back(buf);
+		std::cin >> buf;
+	}
+	
+	int anomaly = 0;
+	double Avg=0;
+	for (int j : heights)
+	{
+		Avg += j;
+	}
+	Avg /= heights.size();
+
+	for (int j : heights)
+	{
+		if (j > Avg) anomaly++;
+	}
+
+	std::cout << "Средний рост: " << Avg << "см."<<std::endl;
+	std::cout << "У " << anomaly << " человек рост превышает средний.";
+		
+}
