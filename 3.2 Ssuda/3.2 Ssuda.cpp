e0b85e1efab1ac8e698c98e208caa681ba848746
MintyRoma <srdhe4@yandex.ru>
2022-11-09 22:32:42 +0300

    Code: Implemented binary search in Task 3.2 *Создано решение с помощью бинарного поиска

--- 3.2 Ssuda/3.2 Ssuda.cpp
+++ 3.2 Ssuda/3.2 Ssuda.cpp
@@ -23,28 +23,28 @@ int main()
 		exit(0);
 	}
 
-	int counted_precision = 0;
+	double calculatedPay = 0;
+	double percent = 50;
+	double previous_percent = 100;
 
-	double percent = 0;
-	double scale = 10;
-
-	while (counted_precision < precision)
+	while (std::abs(calculatedPay - monthpay) > 0.0001)
 	{
-		percent += scale;
-		double r = percent / 100;
-		double calculated_pay = (sum * r * std::pow(1 + r, years)) / (12 * (std::pow(1 + r, years) - 1));
-
-		//DEBUG
-		std::cout <<std::setprecision(precision) << "Percent=" << percent << "  MonthPay=" << calculated_pay << std::endl;
-		std::cout << "Reference:" << monthpay << "   Calculated:" << calculated_pay<<std::endl;
-		std::cout << "Precision:" << counted_precision << std::endl<<std::endl;
-		if (monthpay < calculated_pay)
+		double SomeR = percent / 100;
+		calculatedPay = (sum * SomeR * std::pow(1 + SomeR, years)) / (12 * (std::pow(1 + SomeR, years) - 1));
+		
+		double buf = percent;
+		if (calculatedPay > monthpay)
 		{
-			counted_precision++;
-			percent -= scale;
-			scale /= 10;
+			percent -= std::abs(previous_percent - percent) / 2;
 		}
-		if (monthpay  == calculated_pay) break;
+		else if (calculatedPay < monthpay)
+		{
+			percent += std::abs(previous_percent - percent) / 2;
+		}
+		else break;
+		previous_percent = buf;
+		std::cout << "Percent: " << percent<<std::endl;
+		std::cout << "Calculated pay: " << calculatedPay << "\tReference: " << monthpay<<"\n\n";
 	}
 	std::cout << "Процентная ставка: " <<std::fixed << std::setprecision(precision) << percent;
 
