975f082d8fbe0d858332008524cd0a2f932e0514
MintyRoma <srdhe4@yandex.ru>
2022-11-12 19:46:09 +0300

    Code: Completed Task S1 +Создано и решеное задание S1

---
+++ S1.SpinnerCost/S1.SpinnerCost.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(0, "");
+	std::cout << "Введите стоимость основы спинера: ";
+	int baseCost;
+	std::cin >> baseCost;
+	std::cout << "Введите стоимость одной лопасти: ";
+	int wingCost;
+	std::cin >> wingCost;
+	std::cout << "Введите предполагаемую стоимость спинера: ";
+	int totalCost;
+	std::cin >> totalCost;
+	std::cout << "Максимальное количество лопастей: " << (totalCost - baseCost) / wingCost;
+}
