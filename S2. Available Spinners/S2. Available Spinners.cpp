f3d2a3fd3f9496f034595f19f2e037921ebe926e
MintyRoma <srdhe4@yandex.ru>
2022-11-12 20:08:29 +0300

    Code: completed task S2 +Создано и решено задание S2

---
+++ S2. Available Spinners/S2. Available Spinners.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(0, "");
+	std::cout << "Введите количество лопастей: ";
+	int wings;
+	std::cin >> wings;
+	int threeWings = 0;
+	int fourWings = 0;
+	while (wings != 0)
+	{	
+		if (wings % 4==0)
+		{
+			wings -= 4;
+			fourWings++;
+			continue;
+		}
+		if (wings>=3)
+		{
+			wings -= 3;
+			threeWings++;
+			continue;
+		}
+		else
+		{
+			threeWings = 0;
+			fourWings = 0;
+			wings = 0;
+			break;
+		}
+	}
+	std::cout << threeWings<<std::endl;
+	std::cout << fourWings;
+}
